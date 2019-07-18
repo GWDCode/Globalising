1456f314cd0b431b5fbf63dcdacaa366602eaa97
GWDCode <paj4love@yahoo.com>
2019-07-18 10:59:34 -0700

    My first commit

---
+++ nsasaproject/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from nsasaproject import views 
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    url(r'^$', views.login_redirect, name='login_redirect'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^account/', include('accounts.urls')),
+]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
